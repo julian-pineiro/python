8a6f979e58b1154ab1404663c8deb85f4e7a9401
Julian <julipineiro@gmail.com>
2019-01-27 05:43:34 -0300

    Forgot updating. oops.
    This is it.

--- Tutoring 1.py
+++ Tutoring 1.py
@@ -17,10 +17,8 @@ def remove(name):
             del students[i]
             print("Successfully deleted!\n")
 
-
-
-
-    
+#Insertion
+#Start a loop to insert the whole class. 
 while (keep_inserting==True):
     name = input("Student Name: ") #Input the name
     email = input("Student Email: ") #Input the email
@@ -46,13 +44,53 @@ while (keep_inserting==True):
     if (ans=="n"):
         keep_inserting = False
 
+#Show the remaining students list.
 show()
 
+#Deletion
+#Prompt if the user wants to delete the users that left.
 delete = str(input("Do you wish to delete a student from the list? (y/n) : ")).lower()
-while (delete=="y"):
+while (delete=="y"): #While user wants to delete
     toremove = input("Who do you wish to delete from the list? (name) : ")
-    remove(toremove)
+    remove(toremove) #call the function declared on top.
     delete = str(input("Do you wish to delete another student from the list? (y/n) : ")).lower()
-    show()
+
+
+#Show again the remaining table.
+show()
+
+
+
+#2d Array.
+#The idea behind this is to create an array with the data you need, and store it
+#inside another array. So, when you access an index of students, you actually access
+#another array that contains one specific student's info. Then, accessing that
+#array in indexes 0..3 gives you the student's info field you desire.
+
+#Example:
+name = input("Student name: ")
+email = input("Email address: ")
+dob = input("Date of birth: ")
+std_id = input("Student ID: ")
+
+#Create the student info array
+student_info = [name, email, dob, std_id]
+#add it to the students list.
+students.insert(len(students), student_info)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
